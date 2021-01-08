8b1cf5d2a2156038510102a6af51117d7c819634
Matias Vasconez <68170693+M-Vasconez@users.noreply.github.com>
2021-01-07 23:24:40 -0500

    Se soluciono los problemas de Informacion adicional al profesor y se agrego la clase notas

--- TalllerRefactoring/src/modelos/InformacionAdicionalProfesor.java
+++ TalllerRefactoring/src/modelos/InformacionAdicionalProfesor.java
@@ -5,4 +5,36 @@ public class InformacionAdicionalProfesor {
     public String facultad;
     public double BonoFijo;
     
+    public double calcularSueldo(){
+        double sueldo;
+        sueldo= añosdeTrabajo*600 + BonoFijo;
+        return sueldo;
+    }
+
+    public int getAñosdeTrabajo() {
+        return añosdeTrabajo;
+    }
+
+    public void setAñosdeTrabajo(int añosdeTrabajo) {
+        this.añosdeTrabajo = añosdeTrabajo;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public double getBonoFijo() {
+        return BonoFijo;
+    }
+
+    public void setBonoFijo(double BonoFijo) {
+        this.BonoFijo = BonoFijo;
+    }
+    
+    
+    
 }
---
+++ TalllerRefactoring/src/modelos/Nota.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author Cotrina
+ */
+public class Nota {
+
+    private double nexamen;
+    private double nlecciones;
+    private double ntalleres;
+    private double ndeberes;
+
+    public double getNexamen() {
+        return nexamen;
+    }
+
+    public void setNexamen(double nexamen) {
+        this.nexamen = nexamen;
+    }
+
+    public double getNlecciones() {
+        return nlecciones;
+    }
+
+    public void setNlecciones(double nlecciones) {
+        this.nlecciones = nlecciones;
+    }
+
+    public double getNtalleres() {
+        return ntalleres;
+    }
+
+    public void setNtalleres(double ntalleres) {
+        this.ntalleres = ntalleres;
+    }
+
+    public double getNdeberes() {
+        return ndeberes;
+    }
+
+    public void setNdeberes(double ndeberes) {
+        this.ndeberes = ndeberes;
+    }
+       
+}
+
